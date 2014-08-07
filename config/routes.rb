0330688aa68d7b5c6e10304458dd034e927ae3dd
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-08-07 16:36:29 +0900

    rubocop: Clean up codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Poteti::Engine.routes.draw do
-  devise_for :admin_users, class_name: "Poteti::AdminUser"
+  devise_for :admin_users, class_name: 'Poteti::AdminUser'
   devise_for :users, class_name: 'Poteti::User'
 
   resources :users, param: :name do
