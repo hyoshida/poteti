0f7778ed53fbdcc9a0e63ecc24cfa8ebf6cab3ac
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-08-19 08:28:04 +0900

    Refactor routes.rb for application

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
-  devise_for :admin_users, class_name: 'Poteti::AdminUser', module: :devise
-
-  mount Poteti::Engine, at: '/'
+  Poteti.routes(self)
 end
