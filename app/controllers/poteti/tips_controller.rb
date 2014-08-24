a46e325b428cd30b203d928f4a1d07e5c6a805b0
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-08-24 21:27:03 +0900

    Implement provisional feature to post tips

--- app/controllers/poteti/tips_controller.rb
+++ app/controllers/poteti/tips_controller.rb
@@ -5,5 +5,24 @@ module Poteti
       fail ActiveRecord::RecordNotFound unless @tip
       @user = @tip.user
     end
+
+    def create
+      @tip = current_user.tips.new
+      @tip.attributes = tip_params
+      if @tip.save
+        redirect_to user_tip_path(@tip, user_name: current_user.name)
+      else
+        redirect_to user_tips_path(user_name: current_user.name)
+      end
+    end
+
+    private
+
+    def tip_params
+      params.require(:tip).permit(
+        :title,
+        :body
+      )
+    end
   end
 end
--- app/controllers/poteti/users_controller.rb
+++ app/controllers/poteti/users_controller.rb
@@ -1,8 +1,9 @@
 module Poteti
   class UsersController < ApplicationController
     def show
-      @user = User.find_by(name: params[:name])
+      @user = User.joins(:tips).find_by(name: params[:name])
       fail ActiveRecord::RecordNotFound unless @user
+      @tips = @user.tips
     end
   end
 end
